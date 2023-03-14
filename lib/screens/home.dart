42c751ccbfac73986daa50db9a9e5b70168aa2e8
Anant2410 <anantgarg2410@gmail.com>
2023-03-14 19:06:58 +0530

    second commit

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import '../constraints/colors.dart';
 import '../model/todo.dart';
 import '../widget/todo_item.dart';
